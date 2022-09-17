c900c9b15c99e7dbcf0ae7c9732814f5d23ff466
mesutilica <mesut.ilica@gmail.com>
2022-09-17 12:20:34 +0300

    17-09-2022 v3

---
+++ UrunSatisSitesi.WebUI/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UrunSatisSitesi.Entities;
+using UrunSatisSitesi.Service.Repositories;
+using UrunSatisSitesi.WebUI.Utils;
+
+namespace UrunSatisSitesi.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SlidersController : Controller
+    {
+        private readonly IRepository<Slider> _repository;
+
+        public SlidersController(IRepository<Slider> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: SlidersController
+        public async Task<ActionResult> Index()
+        {
+            var liste = await _repository.GetAllAsync();
+
+            return View(liste);
+        }
+
+        // GET: SlidersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SlidersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SlidersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Slider slider, IFormFile Image)
+        {
+            try
+            {
+                if (Image is not null) slider.Image = await FileHelper.FileLoaderAsync(Image);
+                await _repository.AddAsync(slider);
+                await _repository.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SlidersController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var kayit = await _repository.FindAsync(id);
+
+            return View(kayit);
+        }
+
+        // POST: SlidersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SlidersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SlidersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
